52b5dbca7f0b4cd80b9ec2a3c55b36d3b6c45433
YOSET C <64040965+ycozco@users.noreply.github.com>
2023-10-30 19:35:44 +0000

    updatE

--- trabajo30_10/Ejercicio02.cpp
+++ trabajo30_10/Ejercicio02.cpp
@@ -1,106 +1,88 @@
 #include <stdio.h>
 #include <pthread.h>
 
-// Cada thread cuenta TIMES_TO_COUNT veces
-#define TIMES_TO_COUNT 21000
+#define TIMES_TO_COUNT 21000  // Número de veces que cada hilo incrementará el contador
 #define NC "\e[0m"
 #define YELLOW "\e[33m"
 #define BYELLOW "\e[1;33m"
 #define RED "\e[31m"
 #define GREEN "\e[32m"
 
-// Esta estructura contiene el recuento así como el mutex
-// eso protegerá el acceso a la variable.
-typedef struct s_counter
-{
-    pthread_mutex_t count_mutex;
-    unsigned int count;
+typedef struct s_counter {
+    pthread_mutex_t count_mutex;  // Mutex para sincronizar el acceso al contador
+    unsigned int count;  // Variable contador
 } t_counter;
 
-void *thread_routine(void *data)
-{
-    // Cada thread empieza aqui
+void *thread_routine(void *data) {
     pthread_t tid;
-    t_counter *counter; // puntero a la estructura en main
+    t_counter *counter;
     unsigned int i;
     tid = pthread_self();
     counter = (t_counter *)data;
 
-    // Imprime el recuento antes de que este hilo comience a iterar.
-    // Para leer el valor de count, bloqueamos el mutex:
+    // Bloquear el mutex para leer el valor inicial del contador
     pthread_mutex_lock(&counter->count_mutex);
     printf("%sThread [%ld]: contador del thread empieza = %u.%s\n",
            YELLOW, tid, counter->count, NC);
     pthread_mutex_unlock(&counter->count_mutex);
 
     i = 0;
-    while (i < TIMES_TO_COUNT)
-    {
-        // Iterar TIMES_TO_COUNT veces
-        // Incrementa el contador en cada iteración
-        // Bloquear el mutex mientras dure el incremento
+    while (i < TIMES_TO_COUNT) {
         pthread_mutex_lock(&counter->count_mutex);
         counter->count++;
         pthread_mutex_unlock(&counter->count_mutex);
         i++;
     }
 
-    // Imprime el recuento final cuando este hilo termina su
-    // cuenta propia, sin olvidar bloquear el mutex:
     pthread_mutex_lock(&counter->count_mutex);
     printf("%sThread [%ld]: Contador final = %u.%s\n",
            BYELLOW, tid, counter->count, NC);
     pthread_mutex_unlock(&counter->count_mutex);
-    return (NULL); // Thread termine ici.
+    return (NULL);
 }
 
-int main(void)
-{
-    pthread_t tid1;
-    pthread_t tid2;
+int main(void) {
+    pthread_t tid1, tid2, tid3, tid4;  // IDs de los 4 hilos
 
-    // Esta estructura contiene el contador total del thread
     t_counter counter;
 
-    // Aquí solo hay un hilo (hilo principal), por lo que podemos hacerlo de forma segura
-    // inicializa el recuento sin utilizar el mutex.
     counter.count = 0;
 
-    // Inicializa el mutex :
     pthread_mutex_init(&counter.count_mutex, NULL);
 
-    // Dado que cada hilo cuenta TIMES_TO_COUNT veces y eso
-    // tenemos 2 hilos, esperamos que el recuento final sea
-    // 2 * VECES_PARA_CONTAR:
+    // El contador esperado es 4 * TIMES_TO_COUNT
     printf("Main: el contador esperado es %s%u%s\n", GREEN,
-           2 * TIMES_TO_COUNT, NC);
+           4 * TIMES_TO_COUNT, NC);
 
-    // Creación del Thread:
     pthread_create(&tid1, NULL, thread_routine, &counter);
     printf("Main: Creando el primer thread [%ld]\n", tid1);
 
     pthread_create(&tid2, NULL, thread_routine, &counter);
     printf("Main: Creando el segundo thread [%ld]\n", tid2);
 
-    // Thread adicionado:
+    pthread_create(&tid3, NULL, thread_routine, &counter);
+    printf("Main: Creando el tercer thread [%ld]\n", tid3);
+
+    pthread_create(&tid4, NULL, thread_routine, &counter);
+    printf("Main: Creando el cuarto thread [%ld]\n", tid4);
+
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+    pthread_join(tid4, NULL);
 
     printf("Main: adiciona el primer thread [%ld]\n", tid1);
     printf("Main: adiciona el segundo thread [%ld]\n", tid2);
+    printf("Main: adiciona el tercer thread [%ld]\n", tid3);
+    printf("Main: adiciona el cuarto thread [%ld]\n", tid4);
 
-    // Contador final de la evaluación:
-    // (Aquí podemos leer el conteo sin preocuparnos
-    // el mutex porque todos los hilos se han unido y
-    // no puede haber carrera de datos entre hilos)
-    if (counter.count != (2 * TIMES_TO_COUNT))
+    if (counter.count != (4 * TIMES_TO_COUNT))
         printf("%sMain: ERROR ! Total contador es %u%s\n",
                RED, counter.count, NC);
     else
         printf("%sMain: OK. Total contador es %u%s\n",
                GREEN, counter.count, NC);
 
-    // Destruye el mutex al final del programa:
     pthread_mutex_destroy(&counter.count_mutex);
     return (0);
 }
